66d7396576cdbc98578be2b5c5261221a949d674
Joe Ruggieri <joeruggieri@Joes-MacBook-Air.local>
2015-04-25 18:11:19 +0100

    added select on index for tours

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,32 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Ejs' });
+  var mysql      = require('mysql');
+  var connection = mysql.createConnection({
+    host     : '10.205.252.133',
+    port     : '8889',
+    user     : 'topdeck',
+    password : 'topdeck',
+    database : 'topdeck'
+  });
+
+  connection.connect(function(err) {
+    if (err) {
+      console.error('error connecting: ' + err.stack);
+      return;
+    }
+
+    console.log('connected as id ' + connection.threadId);
+
+    var tours = false;
+    connection.query('SELECT * from tb_bus_stop limit 5', function(err, rows, fields) {
+      if (err) throw err;
+	  res.render('index', {
+	  		tours: rows
+		});
+    });
+  });
+
 });
 
 module.exports = router;
