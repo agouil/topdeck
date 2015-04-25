60150cd4129fe311a475139ded610b5dffb1436c
Andreas Williams <aw2610@doc.ic.ac.uk>
2015-04-25 17:09:02 +0100

    adding route page

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var db = require('./routes/db');
+var route = require('./routes/route');
 
 var app = express();
 
@@ -27,6 +28,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/route', route);
 app.use('/db', db);
 
 // catch 404 and forward to error handler
