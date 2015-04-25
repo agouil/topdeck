d517289df3dc5b1b0e0879cee721f4a9741dd2b5
Henry Searle <henry.searle@gmail.com>
2015-04-25 22:22:15 +0100

    Calculate distance for each spot

--- public/js.js
+++ public/js.js
@@ -38,15 +38,39 @@ google.maps.event.addDomListener(window, 'load', initialize);
 
 
 
-$('.step-container').each(function(steps) {
+$('.step-container').each(function(i, steps) {
     function showPosition(position) {
-        steps.find('').each(function() {
+        $(steps).find('.step').each(function(i, step) {
+            console.log($(step))
+            var lat = $(step).data('lat'),
+                lng = $(step).data('lng'),
+                distance = calcDistance(
+                        lat,
+                        lng,
+                        position.coords.latitude,
+                        position.coords.longitude
+                    );
+
+            $(step).find('.step-distance').html(distance);
 
         });
-        x.innerHTML = "Latitude: " + position.coords.latitude +
-            "<br>Longitude: " + position.coords.longitude;
     }
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(showPosition);
     }
 });
+
+
+function calcDistance(lat1,lon1,lat2,lon2) {
+    var R = 6371; // km (change this constant to get miles)
+    var dLat = (lat2-lat1) * Math.PI / 180;
+    var dLon = (lon2-lon1) * Math.PI / 180;
+    var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+        Math.cos(lat1 * Math.PI / 180 ) * Math.cos(lat2 * Math.PI / 180 ) *
+        Math.sin(dLon/2) * Math.sin(dLon/2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+    var d = R * c;
+    if (d>1) return Math.round(d)+"km";
+    else if (d<=1) return Math.round(d*1000)+"m";
+    return d;
+}
