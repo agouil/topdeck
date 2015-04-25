941392d0b055e6db5076818fae73b9d0e92b6252
Henry Searle <henry.searle@gmail.com>
2015-04-25 22:51:31 +0100

    Queued Queries. Cool

--- routes/route.js
+++ routes/route.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+var q = require('Q');
+
 router.get('/details/:id', function (req, res, next) {
     var mysql = require('mysql');
     var connection = mysql.createConnection({
@@ -20,24 +22,38 @@ router.get('/details/:id', function (req, res, next) {
         console.log('connected as id ' + connection.threadId);
 
         var tourId = req.params.id;
-        var sql = 'select '
-            + 'pk_tour_item as id, Stop_Name as busStopName, Route as busRoute, spot.name as spotName, rank, '
-            + 'img as spotImage, text as spotText, coalesce(stop.lat, spot.lat) as lat, coalesce(stop.lng, spot.lng) as lng '
-            + 'from tb_tour_item '
-            + 'left join tb_bus_sequence as bus on fk_bus_sequence_id = pk_bus_sequence_id '
-            + 'left join tb_bus_stop as stop on bus.Bus_Stop_Code = stop.Bus_Stop_Code '
-            + 'left join tb_spot as spot on fk_spot_id = pk_spot_id '
-            + 'where fk_tour_id = '
-            + tourId
-            + ' order by rank asc';
-        connection.query(sql, function (err, rows, fields) {
-            if (err) throw err;
 
-            var steps = [];
-            for (var i = 0, len = rows.length; i < len; i++) {
-                var row = rows[i];
+        function stepQuery(){
+            var deferred = q.defer();
+            var sql = 'select '
+                + 'pk_tour_item as id, Stop_Name as busStopName, Route as busRoute, spot.name as spotName, rank, '
+                + 'img as spotImage, text as spotText, coalesce(stop.lat, spot.lat) as lat, coalesce(stop.lng, spot.lng) as lng '
+                + 'from tb_tour_item '
+                + 'left join tb_bus_sequence as bus on fk_bus_sequence_id = pk_bus_sequence_id '
+                + 'left join tb_bus_stop as stop on bus.Bus_Stop_Code = stop.Bus_Stop_Code '
+                + 'left join tb_spot as spot on fk_spot_id = pk_spot_id '
+                + 'where fk_tour_id = '
+                + tourId
+                + ' order by rank asc';
+            connection.query(sql, deferred.makeNodeResolver());
+            return deferred.promise;
+        }
+
+        function tourQuery(){
+            var deferred = q.defer();
+            connection.query('SELECT * FROM tb_spot where pk_spot_id = ' + tourId, deferred.makeNodeResolver());
+            return deferred.promise;
+        }
+
+        q.all([stepQuery(),tourQuery()]).then(function(results) {
+            var steps = results[0][0];
+            var tour = results[1][0];
+
+            var returnSteps = [];
+            for (var i = 0, len = steps.length; i < len; i++) {
+                var row = steps[i];
                 if (row.spotName) {
-                    steps.push({
+                    returnSteps.push({
                         id: row.id,
                         name : row.spotName,
                         details: row.spotText,
@@ -49,7 +65,7 @@ router.get('/details/:id', function (req, res, next) {
                     var name = row.busStopName.toLowerCase().replace(/^([a-z\u00E0-\u00FC])|\s+([a-z\u00E0-\u00FC])/g, function($1) {
                         return $1.toUpperCase();
                     });
-                    steps.push({
+                    returnSteps.push({
                         id: row.id,
                         name : 'Number ' + row.busRoute + ' Bus from ' + name,
                         details: 'Bus Stop',
@@ -59,7 +75,11 @@ router.get('/details/:id', function (req, res, next) {
                     })
                 }
             }
-            res.render('route', {tourSteps: steps});
+            res.render('route', {
+                tour : tour,
+                tourSteps: returnSteps
+            });
+
         });
     });
 });
