eceab329c2ca9feb38fd58e1394ac9776d829779
Henry Searle <henry.searle@gmail.com>
2015-04-26 02:38:23 +0100

    Styling changes

--- public/js.js
+++ public/js.js
@@ -1,131 +1,173 @@
-
 var mapsReturned = false,
     mapsQueue = [];
 
 function mapAddToWaitQueue(callback) {
-    if (mapsReturned) {
-        callback();
-    } else {
-        mapsQueue.push(callback);
-    }
+  if (mapsReturned) {
+    callback();
+  } else {
+    mapsQueue.push(callback);
+  }
 }
 
 function initialize() {
-    mapsReturned = true;
-    for (var i = 0; i < mapsQueue.length; i++) {
-        var callback = mapsQueue[i];
-        callback();
-    }
+  mapsReturned = true;
+  for (var i = 0; i < mapsQueue.length; i++) {
+    var callback = mapsQueue[i];
+    callback();
+  }
 }
 google.maps.event.addDomListener(window, 'load', initialize);
 
 
-mapAddToWaitQueue(function() {
-    $('.map-canvas').each(function(i, mapCanvas) {
-        var myLatlng = new google.maps.LatLng(
-                $(mapCanvas).data('lat'),
-                $(mapCanvas).data('lng')
-            ),
-            mapOptions = {
-                zoom: 13,
-                center: myLatlng
-            },
-            map = new google.maps.Map(
-                mapCanvas,
-                mapOptions
+mapAddToWaitQueue(function () {
+  $('.map-canvas').each(function (i, mapCanvas) {
+    var myLatlng = new google.maps.LatLng(
+            $(mapCanvas).data('lat'),
+            $(mapCanvas).data('lng')
+        ),
+        mapOptions = {
+          zoom: 13,
+          center: myLatlng
+        },
+        map = new google.maps.Map(
+            mapCanvas,
+            mapOptions
+        );
+
+    var contentString = $(mapCanvas).data('name');
+
+    var infowindow = new google.maps.InfoWindow({
+      content: contentString
+    });
+
+    var marker = new google.maps.Marker({
+      position: myLatlng,
+      map: map,
+      title: contentString
+    });
+    google.maps.event.addListener(marker, 'click', function () {
+      infowindow.open(map, marker);
+    });
+  });
+});
+
+function populateLocationData() {
+  console.log('getlocation')
+  $('.step-container').each(function (i, steps) {
+    function showPosition(position) {
+      $(steps).find('.step').each(function (i, step) {
+        var lat = $(step).data('lat'),
+            lng = $(step).data('lng'),
+            distance = calcDistance(
+                lat,
+                lng,
+                position.coords.latitude,
+                position.coords.longitude
             );
 
-        var contentString = $(mapCanvas).data('name');
+        $(step).find('.step-distance').html(distance);
 
-        var infowindow = new google.maps.InfoWindow({
-            content: contentString
-        });
-
-        var marker = new google.maps.Marker({
-            position: myLatlng,
-            map: map,
-            title: contentString
-        });
-        google.maps.event.addListener(marker, 'click', function() {
-            infowindow.open(map, marker);
-        });
-    });
-});
-
-
-$('.step-container').each(function(i, steps) {
-    function showPosition(position) {
-        $(steps).find('.step').each(function(i, step) {
-            var lat = $(step).data('lat'),
-                lng = $(step).data('lng'),
-                distance = calcDistance(
-                        lat,
-                        lng,
-                        position.coords.latitude,
-                        position.coords.longitude
-                    );
-
-            $(step).find('.step-distance').html(distance);
-
-        });
+      });
     }
+
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition);
+      navigator.geolocation.getCurrentPosition(showPosition);
     }
-});
+  });
+}
+
+
+function showMapForStep(mapCanvas) {
+  if (mapCanvas.length > 0 && !mapCanvas.hasClass('init')) {
+    mapCanvas.addClass('init');
+    mapAddToWaitQueue(function () {
+      var myLatlng = new google.maps.LatLng(
+              $(mapCanvas).data('lat'),
+              $(mapCanvas).data('lng')
+          ),
+          mapOptions = {
+            zoom: 16,
+            center: myLatlng
+          },
+          map = new google.maps.Map(
+              mapCanvas[0],
+              mapOptions
+          );
+
+      var contentString = $(mapCanvas).data('name');
+
+      var infowindow = new google.maps.InfoWindow({
+        content: contentString
+      });
+
+      var marker = new google.maps.Marker({
+        position: myLatlng,
+        map: map,
+        title: contentString
+      });
+      google.maps.event.addListener(marker, 'click', function () {
+        infowindow.open(map, marker);
+      });
+
+
+      var myloc = new google.maps.Marker({
+        clickable: false,
+        icon: new google.maps.MarkerImage('//maps.gstatic.com/mapfiles/mobile/mobileimgs2.png',
+            new google.maps.Size(22, 22),
+            new google.maps.Point(0, 18),
+            new google.maps.Point(11, 11)),
+        shadow: null,
+        zIndex: 999,
+        map: map // your google.maps.Map object
+      });
+
+      if (navigator.geolocation) navigator.geolocation.getCurrentPosition(function (pos) {
+        var me = new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude);
+        myloc.setPosition(me);
+      });
+
+    });
+  }
+}
 
 $('.step-type-stop .step-details').on('show.bs.collapse', function () {
-    var mapCanvas = $(this).find('.stop-map-canvas');
-    if (!mapCanvas.hasClass('init')) {
-        mapCanvas.addClass('init');
-        mapAddToWaitQueue(function () {
-            var myLatlng = new google.maps.LatLng(
-                    $(mapCanvas).data('lat'),
-                    $(mapCanvas).data('lng')
-                ),
-                mapOptions = {
-                    zoom: 16,
-                    center: myLatlng
-                },
-                map = new google.maps.Map(
-                    mapCanvas[0],
-                    mapOptions
-                );
-
-            var contentString = $(mapCanvas).data('name');
-
-            var infowindow = new google.maps.InfoWindow({
-                content: contentString
-            });
-
-            var marker = new google.maps.Marker({
-                position: myLatlng,
-                map: map,
-                title: contentString
-            });
-            google.maps.event.addListener(marker, 'click', function () {
-                infowindow.open(map, marker);
-            });
-        });
-    }
+  showMapForStep($(this).find('.stop-map-canvas'));
 });
 
-function calcDistance(lat1,lon1,lat2,lon2) {
-    var R = 6371; // km (change this constant to get miles)
-    var dLat = (lat2-lat1) * Math.PI / 180;
-    var dLon = (lon2-lon1) * Math.PI / 180;
-    var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
-        Math.cos(lat1 * Math.PI / 180 ) * Math.cos(lat2 * Math.PI / 180 ) *
-        Math.sin(dLon/2) * Math.sin(dLon/2);
-    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-    var d = R * c;
-    if (d>1) return Math.round(d)+"km";
-    else if (d<=1) return Math.round(d*1000)+"m";
-    return d;
+function calcDistance(lat1, lon1, lat2, lon2) {
+  var R = 6371; // km (change this constant to get miles)
+  var dLat = (lat2 - lat1) * Math.PI / 180;
+  var dLon = (lon2 - lon1) * Math.PI / 180;
+  var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+      Math.cos(lat1 * Math.PI / 180) * Math.cos(lat2 * Math.PI / 180) *
+      Math.sin(dLon / 2) * Math.sin(dLon / 2);
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  var d = R * c;
+  if (d > 1) return Math.round(d) + "km";
+  else if (d <= 1) return Math.round(d * 1000) + "m";
+  return d;
+}
+
+function goToNextStep() {
+  var currentStep = $('.step.current-step').removeClass('current-step');
+  // hide the details
+  currentStep.find('.step-details').addClass('collapse');
+  console.log(currentStep.find('.step-bar'));
+  var nextStep = $(currentStep.next()[0]);
+  nextStep.addClass('current-step');
+  // show the details of the next step
+  nextStep.find('.step-details').collapse('show');
+  // scroll down to the next step
+  var container = $('html,body'),
+      navbarHeight = 53;
+  container.animate({
+    scrollTop: nextStep.offset().top - container.offset().top + container.scrollTop() - navbarHeight
+  });
 }
 
 // Calling
-$(document).ready(function() {
+$(document).ready(function() {showMapForStep($('.step-details:not(.collapse) .stop-map-canvas'));
+
     if ($('#callRequest')) {
         $('#callRequest').click(function() {
             $.ajax({
@@ -157,6 +199,57 @@ $(document).ready(function() {
           });
         });
 
-      }
+  $('.start-tour .btn').click(function() {
+    goToNextStep();
+    $(this).parent('.start-tour').hide();
+    $('.next-stop').show();
+    var currentStep = $('.step.current-step');
+    $('.next-stop .step-distance').html(
+        currentStep.find('.step-distance').html()
+    );
+  });
 
+  $('.next-stop .btn').click(function() {
+    goToNextStep();
+    var currentStep = $('.step.current-step');
+    $('.next-stop .step-distance').html(
+        currentStep.find('.step-distance').html()
+    );
+  });
+
+  if ($('#tour').length > 0) {
+    populateLocationData();
+
+    setInterval(populateLocationData, 5000);
+  }
+
+/*
+  if ($('#callRequest')) {
+    $('#callRequest').click(function () {
+      $.ajax({
+        url: '/tourtext/' + $('#callRequest').data('route-id'),
+        cache: false,
+        timeout: 5000,
+        success: function (data) {
+        }
+      });
+    });
+  }
+  if ('#btn_detail_purchase') {
+    $('#btn_detail_purchase').click(function () {
+      var id = $(this).attr('data-id');
+      var url = '/payment/tour/' + id
+      $.ajax({
+        url: url
+      }).success(function (response) {
+        $('#btn_detail_purchase').addClass('hidden');
+        $('#paymentContainer').html(response);
+        $('#payment_cancel_button').click(function () {
+          $('#paymentContainer').empty();
+          $('#btn_detail_purchase').removeClass('hidden');
+        });
+      });
+    });
+
+  }*/
 });
--- routes/route.js
+++ routes/route.js
@@ -4,94 +4,101 @@ var router = express.Router();
 var q = require('Q');
 
 router.get('/details/:id', function (req, res, next) {
-    var mysql = require('mysql');
-    var connection = mysql.createConnection({
-        host: '10.205.252.133',
-        port: '8889',
-        user: 'topdeck',
-        password: 'topdeck',
-        database: 'topdeck'
+  var mysql = require('mysql');
+  var connection = mysql.createConnection({
+    host: '10.205.252.133',
+    port: '8889',
+    user: 'topdeck',
+    password: 'topdeck',
+    database: 'topdeck'
+  });
+
+  connection.connect(function (err) {
+    if (err) {
+      console.error('error connecting: ' + err.stack);
+      return;
+    }
+
+    console.log('connected as id ' + connection.threadId);
+
+    var tourId = req.params.id;
+
+    function stepQuery() {
+      var deferred = q.defer();
+      var sql = 'select '
+          + 'pk_tour_item as id, spot.pk_spot_id as spotId, Stop_Name as busStopName, Route as busRoute, spot.name as spotName, rank, '
+          + 'img as spotImage, text as spotText, coalesce(stop.lat, spot.lat) as lat, coalesce(stop.lng, spot.lng) as lng '
+          + 'from tb_tour_item '
+          + 'left join tb_bus_sequence as bus on fk_bus_sequence_id = pk_bus_sequence_id '
+          + 'left join tb_bus_stop as stop on bus.Bus_Stop_Code = stop.Bus_Stop_Code '
+          + 'left join tb_spot as spot on fk_spot_id = pk_spot_id '
+          + 'where fk_tour_id = '
+          + tourId
+          + ' order by rank asc';
+      connection.query(sql, deferred.makeNodeResolver());
+      return deferred.promise;
+    }
+
+    function tourQuery() {
+      var deferred = q.defer();
+      connection.query('SELECT * FROM tb_tour where pk_tour_id = ' + tourId, deferred.makeNodeResolver());
+      return deferred.promise;
+    }
+
+    q.all([stepQuery(), tourQuery()]).then(function (results) {
+      connection.destroy();
+      var steps = results[0][0];
+      var tour = results[1][0][0];
+
+      try{
+      var returnSteps = [];
+      for (var i = 0, len = steps.length; i < len; i++) {
+        var row = steps[i];
+        if (row.spotName) {
+          returnSteps.push({
+            id: row.id,
+            ref: row.spotId,
+            name: row.spotName,
+            details: row.spotText,
+            image: row.spotImage,
+            lng: row.lng,
+            lat: row.lat,
+            type: 'spot'
+          })
+        } else {
+          var name = row.busStopName.toLowerCase().replace(/^([a-z\u00E0-\u00FC])|\s+([a-z\u00E0-\u00FC])/g, function ($1) {
+            return $1.toUpperCase();
+          });
+          returnSteps.push({
+            id: row.id,
+            ref: row.spotId,
+            name: 'Number ' + row.busRoute + ' Bus from ' + name,
+            details: 'map',
+            image: '/img/bus.jpg',
+            lng: row.lng,
+            lat: row.lat,
+            type: 'stop'
+          })
+        }
+      }
+      } catch (err) {console.log(err)}
+      res.render('route', {
+        tour: tour,
+        tourSteps: returnSteps
+      });
+
+      // Insert call text
+      callText = callText.replace(/['"]+/g, '');
+      var sqlInsertCallText = 'insert into tb_tour_call_text (text, fk_tour_id) '
+          + 'values ("' + callText + '", ' + tourId + ')';
+      connection.query(sqlInsertCallText, function (err, rows, fields) {
+        if (err) throw err;
+        res.render('route', {tourSteps: steps});
+
+        connection.destroy();
+      });
     });
-
-    connection.connect(function (err) {
-        if (err) {
-            console.error('error connecting: ' + err.stack);
-            return;
-        }
-
-        console.log('connected as id ' + connection.threadId);
-
-        var tourId = req.params.id;
-
-        function stepQuery(){
-            var deferred = q.defer();
-            var sql = 'select '
-                + 'pk_tour_item as id, Stop_Name as busStopName, Route as busRoute, spot.name as spotName, rank, '
-                + 'img as spotImage, text as spotText, coalesce(stop.lat, spot.lat) as lat, coalesce(stop.lng, spot.lng) as lng '
-                + 'from tb_tour_item '
-                + 'left join tb_bus_sequence as bus on fk_bus_sequence_id = pk_bus_sequence_id '
-                + 'left join tb_bus_stop as stop on bus.Bus_Stop_Code = stop.Bus_Stop_Code '
-                + 'left join tb_spot as spot on fk_spot_id = pk_spot_id '
-                + 'where fk_tour_id = '
-                + tourId
-                + ' order by rank asc';
-            connection.query(sql, deferred.makeNodeResolver());
-            return deferred.promise;
-        }
-
-        function tourQuery(){
-            var deferred = q.defer();
-            connection.query('SELECT * FROM tb_spot where pk_spot_id = ' + tourId, deferred.makeNodeResolver());
-            return deferred.promise;
-        }
-
-        q.all([stepQuery(),tourQuery()]).then(function(results) {
-            var steps = results[0][0];
-            var tour = results[1][0][0];
-
-            var returnSteps = [];
-            for (var i = 0, len = steps.length; i < len; i++) {
-                var row = steps[i];
-                if (row.spotName) {
-                    returnSteps.push({
-                        id: row.id,
-                        name : row.spotName,
-                        details: row.spotText,
-                        image : row.spotImage,
-                        lng : row.lng,
-                        lat : row.lat,
-                        type: 'spot'
-                    })
-                } else {
-                    var name = row.busStopName.toLowerCase().replace(/^([a-z\u00E0-\u00FC])|\s+([a-z\u00E0-\u00FC])/g, function($1) {
-                        return $1.toUpperCase();
-                    });
-                    returnSteps.push({
-                        id: row.id,
-                        name : 'Number ' + row.busRoute + ' Bus from ' + name,
-                        details: 'map',
-                        image : '/img/bus.jpg',
-                        lng : row.lng,
-                        lat : row.lat,
-                        type: 'stop'
-                    })
-                }
-            }
-            res.render('route', {
-                tour : tour,
-                tourSteps: returnSteps
-            });
-
-            // Insert call text
-            callText = callText.replace(/['"]+/g, '');
-            var sqlInsertCallText = 'insert into tb_tour_call_text (text, fk_tour_id) '
-              + 'values ("' + callText + '", ' + tourId + ')';
-            connection.query(sqlInsertCallText, function (err, rows, fields) {
-                if (err) throw err;
-                res.render('route', {tourSteps: steps});
-            });
-	    });
-    });
+  });
 });
 
 module.exports = router;
